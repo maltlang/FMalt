8d09c7c4da97bfb7877dc3b35457c5eb3a21fe1e
刘知杭 <enterindex@gmail.com>
2019-04-17 18:07:07 +0800

    update

--- Precompiler/Library.fs
+++ Precompiler/Library.fs
@@ -1,146 +1,38 @@
-namespace Precompiler
+﻿namespace Precompiler
 
 module Precompiler =
     open MParser.MParser
     open LParserC.LParserC
-
-    type Body =
-    | Const of RValue
-    | Let of (RValue * BodyNode * BodyNode list)
-    | Use of (Body * string * BodyNode * BodyNode list)
-    | Cond of (BodyNode * BodyNode) list
-    //| Match of (Body)
-    | Quote of RValue
-    | Lambda of (MataTree * BodyNode)
-    | DyLoad of string
-    | FunCall
-    //| MacroCall
-
-    and BodyNode = (Pos * Body)
-
-    (*
-    type MacroCall = {
-        callname: string
-        parameter: MataTree[]
-    }
-    *)
-
-    type typeLabel =
-    | Any
-    | Or of (typeLabel * typeLabel)
-    | Nil
-    | Int
-    | Uint
-    | Float
-    | Bool
-    | Char
-    | String
-    | List of typeLabel
-    | Tuple of typeLabel list
-    //| Struct of string
-    | Function of (typeLabel * typeLabel)
-
-    let rec Equ self o2 =
-        match (self, o2) with
-        | (Any, _) | (_, Any) -> true
-        | (Or (x1, x2), Or (y1, y2)) -> (Equ x1 y1 && Equ x2 y2) || (Equ x1 y2 && Equ x2 y1)
-        | (List (x), List (y)) -> Equ x y
-        | (Tuple (x), Tuple (y)) -> rtrt x y
-        | (Function (x, y), Function (x2, y2)) -> Equ x y && Equ x2 y2
-        | (x, y) when x = y -> true
-        | _ -> false
-
-    and rtrt t1 t2 =
-        match (t1, t2) with
-        | (v::t, v2::t2) ->
-            if Equ v v2
-            then rtrt t t2
-            else false
-        | ([], []) -> true
-        | _ -> false
-
-    type TopAst =
-    | Nil
-    | Import of string list
-    | Load of string list
-    | Export of string list
-    | Defun of (string * MataTree * BodyNode list)
-    | DefMacro of (string * MataTree * BodyNode list)
-    | TypeLabel of typeLabel
-    | MacroCall of (string * MataTree)
-
-    type rootNode = {
-        pos: LParserC.LParserC.Pos
-        ast: TopAst
-    }
-
-    type compilerContext = {
-        a: int
-    }
-
-    (*
-    // macro eval
-    let pureEval (context: CompilerContext) (args: MataTree) =
-        0
-    *)
+    //open Map
 
     type InvalidTypeLabel(pos: Pos) = inherit System.ApplicationException()
     type ParamerIsNotSymbol(pos: Pos) = inherit System.ApplicationException()   
     type InvalidTopLevelExpr(pos: Pos) = inherit System.ApplicationException()
 
-    let rec getBodyNode (args: MataTree) = 0
+    type ExprValue =
+    | Nil
+    | Const of RValue
+    | Symbol of string
+    | Set of (string * Expr)
+    | Let of (string * Expr * Expr list)
+    | Use of (string * Expr * Expr list)
+    | Lambda of (string list * Expr list)
+    | MethodCall of (Expr * string * Expr list)
+    
+    and Expr = (ExprValue * Pos)
 
-    let rec getTypeLabel (args: MataTree) =
+    let rec prec (args: MataTree): Expr =
         match args.valu with
-        | Symbol (s) when s = "any" -> typeLabel.Any
-        | Symbol (s) when s = "nil" -> typeLabel.Nil
-        | Symbol (s) when s = "int" -> typeLabel.Int
-        | Symbol (s) when s = "uint" -> typeLabel.Uint
-        | Symbol (s) when s = "float" -> typeLabel.Float
-        | Symbol (s) when s = "bool" -> typeLabel.Bool
-        | Symbol (s) when s = "char" -> typeLabel.Char
-        | Symbol (s) when s = "string" -> typeLabel.String
-        | RValue.List ([{valu=Symbol("|")}; t1; t2]) ->
-            typeLabel.Or ((getTypeLabel t1), (getTypeLabel t2))
-        | RValue.List ([{valu=Symbol("list")}; t]) ->
-            typeLabel.List (getTypeLabel t)
-        | RValue.List ([{valu=Symbol("fun")}; pt; rt]) ->
-            typeLabel.Function ((getTypeLabel pt), (getTypeLabel rt))
-        | RValue.List (t) ->
-            typeLabel.Tuple ((List.map getTypeLabel) t)
-        | _ -> raise (InvalidTypeLabel args.pos)
-
-    let getSymbol (args: MataTree) =
-        match args.valu with
-        | Symbol (s) -> s
-        | _ -> raise (ParamerIsNotSymbol args.pos)
-
-    let prec (context: compilerContext) (args: MataTree): rootNode =
-        match args.valu with
-        | RValue.Nil -> {
-            pos= args.pos;
-            ast = Nil}
-        | RValue.List ({valu=Symbol("import")}::t) -> {
-            pos= args.pos;
-            ast= Import ((List.map getSymbol) t)}
-        | RValue.List ({valu=Symbol("load")}::t) -> {
-            pos= args.pos;
-            ast= Load ((List.map getSymbol) t)}
-        | RValue.List ({valu=Symbol("export")}::t) -> {
-            pos= args.pos;
-            ast= Export ((List.map getSymbol) t)}
-        | RValue.List ([{valu=Symbol("type")}; tp]) -> {
-            pos= args.pos;
-            ast= TypeLabel (getTypeLabel tp)}
-            (*
-        | RValue.List ({valu=Symbol("fun")} :: {valu=Symbol(name)} :: p ::t) -> {
-            pos= args.pos;
-            ast= Defun (name, p, (List.map getBodyNode) t)}
-        | RValue.List ({valu=Symbol("macro")} :: {valu=Symbol(name)} :: p ::t) -> {
-            pos= args.pos;
-            ast= Defun (name, p, (List.map getBodyNode) t)}
-            *)
-        | RValue.List ({valu=Symbol(macroName)}::t) -> {
-            pos= args.pos;
-            ast= MacroCall (macroName, {pos= args.pos; valu= RValue.List t})}
+        | RValue.Nil -> (Nil, args.pos)
+        | RValue.Bool (_)
+        | RValue.Char (_)
+        | RValue.Int (_)
+        | RValue.Uint (_)
+        | RValue.Float (_)
+        | RValue.String (_) -> (Const args.valu, args.pos)
+        | RValue.Symbol (x) -> (Symbol x, args.pos)
+        | RValue.List ([{valu=RValue.Symbol("set")}; {valu=RValue.Symbol(name)}; e]) ->
+            (Set (name, prec e), args.pos)
+        | RValue.List ([{valu=RValue.Symbol("set")}; {valu=RValue.Symbol(name)}; e]) ->
+            (Set (name, prec e), args.pos)
         | _ -> raise (InvalidTopLevelExpr args.pos)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FMalt [![Build status](https://ci.appveyor.com/api/projects/status/ww57vjbdnyhhf2tf?svg=true)](https://ci.appveyor.com/project/Zhihang-Liu/fmalt-v9ct3)
-Not Malt(no
+
+FMalt(Functional Malt) is Not Functional Programming Language
+
+FMalt is Objective-Malt
+
+FMalt is a Pure OO(Object Oriented) Programming Language
