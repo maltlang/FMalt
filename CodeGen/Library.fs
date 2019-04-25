a1a0e4c16737b3c5592bc68c0625a39ab0e09210
刘知杭 <enterindex@gmail.com>
2019-04-25 17:18:09 +0800

    updata

--- CodeGen/Library.fs
+++ CodeGen/Library.fs
@@ -1,11 +1,97 @@
 namespace CodeGen
 
-open Precompiler
-
-type ModuleImages = {
-    a: int
-}
-
 module CodeGen =
-    let CodeGenerator (e: Precompiler.Expr): ModuleImages =
-        {a=0}
+
+    open Precompiler.Precompiler
+    open MParser
+    open System.Collections.Generic
+    open LParserC.LParserC
+    open System.Text
+
+    type MethodStruct = {
+        a: int
+    }
+
+    type ModuleContext = {
+        symbol_table: string []
+        const_area: MParser.RValue []
+        method_area: MethodStruct []
+        global_method: MethodStruct
+    }
+
+    type OpValue =
+    | Val1v of uint64
+    | Val2v of (uint32 * uint32)
+
+    type OpCode = (uint32 * byte * OpValue)
+
+    // opcodes.rawMir
+    let load_const          = byte 0
+    let load_lambda         = byte 1
+    let load_symbol         = byte 2    
+    let write_local_symbol  = byte 3
+    let write_object_symbol = byte 4
+    let get_method          = byte 5
+    let copy_to_shared_area = byte 6
+    let delete              = byte 7
+    let gc_new              = byte 8
+    let call                = byte 9
+    let _return             = byte 10
+
+    // opcodes.optimizMir
+    let load_local_of_offset    = byte 11
+    let load_object_of_offset   = byte 12
+    let write_local_of_offset   = byte 13
+    let write_object_of_offset  = byte 14
+    let get_method_of_offset    = byte 15
+
+    //opcodes.optimizL2Mir
+    let throw   = byte 16
+    let add     = byte 17
+    let sub     = byte 18
+    let mul     = byte 19uy
+    let div     = byte 20
+    let _mod    = byte 21
+
+    // append
+    let call_of_offset          = byte 22
+
+    let inline rlsConst env x (p: Pos) =
+        Some ({
+            symbol_table    = env.symbol_table;
+            const_area      = Array.append env.const_area [| x |] ;
+            method_area     = env.method_area;
+            global_method   = env.global_method},
+            [| (p.line, load_const, env.const_area.Length - 1) |])
+
+    let inline rlsSymbol env x (p: Pos) =
+        try
+                let i = env.symbol_table |> Array.findIndex (fun a -> x = a)
+                Some ({
+                symbol_table    = env.symbol_table;
+                const_area      = env.const_area;
+                method_area     = env.method_area;
+                global_method   = env.global_method}, [| (p.line, load_symbol, i) |] )
+            with
+            | :? KeyNotFoundException ->
+                Some ({
+                symbol_table=   Array.append env.symbol_table [| x |];
+                const_area=     env.const_area;
+                method_area=    env.method_area;
+                global_method=  env.global_method}, [| (p.line, load_symbol, env.symbol_table.Length - 1) |])
+
+    let rec CodeGenerator (env: ModuleContext) ((e, p): Expr) =
+        match e with
+        | ExprValue.Const (x) ->
+            rlsConst env x p
+        | ExprValue.Symbol (x) ->
+            rlsSymbol env x p
+        | ExprValue.Set (name, expr) ->
+            match CodeGenerator env expr with
+            | Some (env, codes) ->
+                let (env, codes2)   = (rlsSymbol env name p).Value
+                let (_, _, offset)  = Array.get codes2 0
+                Some (env, (Array.append codes [| (p.line, write_object_symbol, offset) |]))
+            | None -> failwith "AstError: '(set <id> <expr>)' expr is None"
+        | _ -> None
+    
---
+++ CodeGen/images.md
@@ -0,0 +1,58 @@
+﻿# Images
+
+(b = byte; bi = bit)
+
+- magic: 8b (0x039963)
+- symbol-table:
+  - length: 8b
+    - strings: length * string
+      - string.length: 8b
+      - string.bytes: length * 1b
+- const-area:
+  - length: 8b
+    - values: length * value
+      - value: ?
+- global-method: method
+
+## MIR.images
+
+- line: 32bi
+- opcode: 1b
+- value: 8b
+
+### MIR.rawMir (`core` and `std` runtime)
+
+- load_const            `<module-constant-area>`
+- load_lambda           `<module-method-area>`
+- load_symbol           `<symbol-table-offset>`
+- write_local_symbol    `<symbol-table-offset>`
+- write_object_symbol   `<symbol-table-offset>`
+
+- get_method            `<symbol-table-offset>`
+
+- copy_to_shared_area
+- delete
+- gc_new
+
+- call                  `<symbol-table-offset>`
+- return
+
+### MIR.optimizMir
+
+- load_local_of_offset      `<uint64>`
+- load_object_of_offset     `<uint64>`
+- write_local_of_offset     `<uint64>`
+- write_object_of_offset    `<uint64>`
+
+- get_method_of_offset      `<uint64>`
+
+- call_of_offset            `<uint64>`
+
+### MIR.optimizL2Mir
+
+- throw
+- add   `<uint32>` `<uint32>`
+- sub   `<uint32>` `<uint32>`
+- mul   `<uint32>` `<uint32>`
+- div   `<uint32>` `<uint32>`
+- mod   `<uint32>` `<uint32>`
